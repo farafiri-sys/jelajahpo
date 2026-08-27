1be8770c4904790a3f7690ac83f7e5ef5c618db3
farafiri-sys <firifara44@gmail.com>
2026-08-27 09:40:02 +0700

    sesi 1 dan 2

---
+++ jelajahpo-backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database JelajahPo')
+    }
+})
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang di JelajahPo API 💄');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server JelajahPo jalan di http://localhost:${PORT}`);
+});
+
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
