296405776807714691d674ff4e9b715113b6ad67
farafiri-sys <firifara44@gmail.com>
2026-09-07 08:23:10 +0700

    sesi 10

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const authJWT = require('./middleware');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -63,6 +66,38 @@ app.post('/pengguna', async (req, res) => {
     }
 });
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const sql = 'SELECT * FROM pengguna WHERE email = ?';
+
+    db.query(sql, [email], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Akun tidak ditemukan' });
+        }
+
+        const user = result[0];
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        if (!passwordIsValid) {
+            return res.status(401).json({ message: 'Password salah' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id_pengguna },
+            'jelajahporahasia',
+            { expiresIn: 86400 }
+        );
+
+        res.status(200).json({
+            auth: true,
+            token,
+            id_pengguna: user.id_pengguna,
+            nama: user.nama
+        });
+    });
+});
+
 app.get('/wisata', (req, res) => {
     const sql = 'SELECT * FROM wisata';
     db.query(sql, (err, results) => {
@@ -97,7 +132,7 @@ app.post('/wisata', (req, res) => {
     });
 });
 
-app.put('/wisata/:id_wisata', (req, res) => {
+app.put('/wisata/:id_wisata', authJWT, (req, res) => {
     const { id_wisata } = req.params;
     const { nama_wisata, deskripsi, harga_tiket, id_kategori } = req.body;
 
@@ -116,7 +151,7 @@ app.put('/wisata/:id_wisata', (req, res) => {
     });
 });
 
-app.delete('/wisata/:id_wisata', (req, res) => {
+app.delete('/wisata/:id_wisata', authJWT, (req, res) => {
     const { id_wisata } = req.params;
     const sql = 'DELETE FROM wisata WHERE id_wisata=?';
     db.query(sql, [id_wisata], (err, result) => {
